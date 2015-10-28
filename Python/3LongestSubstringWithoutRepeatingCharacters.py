26bcc55805641b6f2114f318949988cd7f098f96
Qiumy <minyi_qiu@163.com>
2015-10-28 10:47:13 +0800

    add solution of Q3

--- Python/3LongestSubstringWithoutRepeatingCharacters.py
+++ Python/3LongestSubstringWithoutRepeatingCharacters.py
@@ -1,1 +1,17 @@
 __author__ = 'Minyi'
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        maxLength = 0
+        start = 0
+        d = {}
+
+        for i, ch in enumerate(s):
+            if ch in d and d[ch] >= start:
+                start = d[ch] + 1
+            d[ch] = i
+            maxLength = max(maxLength,i-start+1)
+        return maxLength
