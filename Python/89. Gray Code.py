871811657271e1b7a252728e4db61ee9aef21222
qiumy <minyi_qiu@163.com>
2016-02-14 19:20:47 +0800

    solution of 86-90

---
+++ Python/89. Gray Code.py
@@ -0,0 +1,22 @@
+# The gray code is a binary numeral system where two successive values differ in only one bit.
+#
+# Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code. A gray code sequence must begin with 0.
+#
+# For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
+#
+# 00 - 0
+# 01 - 1
+# 11 - 3
+# 10 - 2
+
+class Solution(object):
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        ans = []
+        size = 1 << n
+        for i in range(size):
+            ans.append((i >> 1) ^ i)
+        return ans
